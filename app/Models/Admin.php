dd66fdd88209c207b0d269c62749125a0982dbdc
unknown <shamvil89@gmail.com>
2022-11-14 12:29:08 +0600

    add passport

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,22 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 use Laravel\Passport\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class Admin extends Authenticatable
 {
     use HasFactory, Notifiable, HasApiTokens, HasRoles;    
-    
+    protected $guard = 'admin';    
+
     protected $fillable = [
         'name', 'email', 'password',
     ];    
-    
     protected $hidden = [
-      'password', 'remember_token',
+        'password', 'remember_token',
     ];
 }
